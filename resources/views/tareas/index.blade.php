4d11874cdb1b1b3262547d3af7f0e7fc7203e81c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-07 17:51:37 -0300

    Add form

--- resources/views/tareas/index.blade.php
+++ resources/views/tareas/index.blade.php
@@ -1,5 +1,15 @@
 @extends('app')
 
 @section('content')
-Hello World
+<div class="container w-25 border rounded border-primary my-5 p-2">
+    <form>
+        <div class="mb-3">
+          <label for="title class="form-label">Titulo de tarea</label>
+          <input type="text" class="form-control" id="title" name="title">
+        </div>
+       <div class="mx-auto text-center">
+        <button type="submit" class="btn btn-primary">Crear nueva tarea</button>
+       </div>
+      </form>
+</div>
 @endsection
