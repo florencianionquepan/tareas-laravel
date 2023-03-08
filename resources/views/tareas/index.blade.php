3662a3692bd6875ccbe7c367d7242747b9629a90
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-07 21:00:57 -0300

    Update index.blade.php

--- resources/views/tareas/index.blade.php
+++ resources/views/tareas/index.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 <div class="container w-25 border rounded border-primary my-5 p-2">
-    <form>
+    <form action="{{route('lista')}}" method="POST">
+      @csrf
         <div class="mb-3">
           <label for="title class="form-label">Titulo de tarea</label>
           <input type="text" class="form-control" id="title" name="title">
