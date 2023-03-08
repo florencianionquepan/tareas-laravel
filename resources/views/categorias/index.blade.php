337ce4b580649f4a1ff45b5443ca80d1469aeb5d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-08 20:28:40 -0300

    Update index.blade.php

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -26,6 +26,52 @@
               <button type="submit" class="btn btn-primary">Nueva categoria</button>
              </div>
             </form>
+
+            <div>
+                @foreach ($categorias as $cat)
+                    <div class="row py-3">
+        
+                        <div class="row py-1">
+                            <div class="col-md-9 d-flex align-items-center">
+                                <a class="d-flex align-items-center gap-2" 
+                                href="{{ route('categorias.show', ['categoria' => $cat->id]) }}">
+                                    <span class="color-container" 
+                                    style="background-color: {{ $cat->color }}"></span> {{ $cat->name }}
+                                </a>
+                            </div>
+            
+                            <div class="col-md-3 d-flex justify-content-end">
+                                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal" 
+                                    data-bs-target="#modal{{$cat->id}}">Eliminar</button>
+                                
+                            </div>
+
+                            <!-- MODAL -->
+                        <div class="modal fade" id="modal{{$cat->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Eliminar categoría</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                <div class="modal-body">
+                                    Al eliminar la categoría <strong>{{ $cat->name }}</strong> se eliminan todas las tareas asignadas a la misma. 
+                                    ¿Está seguro que desea eliminarla?
+                                </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            <form action="{{ route('categorias.destroy', ['categoria' => $cat->id]) }}" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-danger">Eliminar</button>
+                            </form>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                @endforeach
+              </div>
     </div>
 </div>
 
