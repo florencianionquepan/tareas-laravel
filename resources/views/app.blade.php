031db6e637e2785582245848179dabe83604e381
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-08 20:07:09 -0300

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,18 +19,17 @@
               </a>
         
               <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" class="nav-link px-2 text-secondary">Inicio</a></li>
+                <li><p class="nav-link px-2 text-secondary pb-1 mb-1">Mis tareas</p></li>
                 <li><a href="#" class="nav-link px-2 text-white">Categorias</a></li>
                 <li><a href="#" class="nav-link px-2 text-white">Tareas</a></li>
               </ul>
         
               <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
+                
               </form>
         
               <div class="text-end">
-                <button type="button" class="btn btn-outline-light me-2">Login</button>
-                <button type="button" class="btn btn-warning">Sign-up</button>
+
               </div>
             </div>
           </div>
