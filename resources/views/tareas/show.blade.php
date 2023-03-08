ade2bde7e086bffc2cd9f1aacf4d8fa17737e116
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-08 18:22:00 -0300

    Create show.blade.php

---
+++ resources/views/tareas/show.blade.php
@@ -0,0 +1,27 @@
+@extends('app')
+
+@section('content')
+<div class="container w-25 border rounded border-primary my-5 p-2">
+    <form action="{{ route('tareas-edit',['id'=> $tarea->id]) }}" method="POST">
+      @csrf
+
+      @if (session('success'))
+        <h6 class="alert alert-success">{{session('success')}}</h6>
+      @endif
+
+      @error('title')
+      <h6 class="alert alert-danger">{{$message}}</h6>
+      @enderror
+
+        <div class="mb-3">
+          <label for="title class="form-label">Titulo de tarea</label>
+          <input type="text" class="form-control" id="title" 
+                name="title" value="{{$tarea->title}}">
+        </div>
+       <div class="mx-auto text-center">
+        <button type="submit" class="btn btn-primary">Editar tarea</button>
+       </div>
+      </form>
+
+</div>
+@endsection
