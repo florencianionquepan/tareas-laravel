abdfd58b0f96b319d4cfe0d04d69895a2ca9ca7f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-08 20:46:12 -0300

    Create show.blade.php

---
+++ resources/views/categorias/show.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('content')
+
+<div class="container w-25 border rounded border-primary my-5 p-2">
+    <div class="row mx-auto">
+        
+    </div>
+</div>
+
+@endsection
