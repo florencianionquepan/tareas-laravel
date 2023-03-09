10f617ff69e3c64ba09f4557d0a250f3fc8d6031
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-08 21:25:11 -0300

    Update show.blade.php

--- resources/views/categorias/show.blade.php
+++ resources/views/categorias/show.blade.php
@@ -4,6 +4,34 @@
 
 <div class="container w-25 border rounded border-primary my-5 p-2">
     <div class="row mx-auto">
+        <form action="{{route('categorias.update',['categoria'=>$categoria->id])}}" method="POST">
+            @method('PATCH')
+            @csrf
+      
+            @if (session('success'))
+              <h6 class="alert alert-success">{{session('success')}}</h6>
+            @endif
+      
+            @error('name')
+                <h6 class="alert alert-danger">{{$message}}</h6>
+            @enderror
+
+              <div class="mb-3">
+                <label for="name" class="form-label">Nombre de categoria</label>
+                <input type="text" class="form-control" name="name" value="{{$categoria->name}}">
+              </div>
+
+              <div class="mb-3">
+                <label for="color" class="form-label">Color</label>
+                <input type="color" class="form-control" name="color" value="{{$categoria->color}}">
+              </div>
+
+             <div class="mx-auto text-center">
+              <button type="submit" class="btn btn-primary">Editar Categoria</button>
+             </div>
+
+        </form>
+
         <div>
             @if($categoria->tareas->count()>0)
                 <div class="row py-2">
@@ -23,6 +51,7 @@
                 No hay tareas para la categoria
             @endif
         </div>
+
     </div>
 </div>
 
