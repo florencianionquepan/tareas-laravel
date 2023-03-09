f62fbb2a392f1a9ddbbcbe4cdeaac269cbd10619
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-08 21:24:32 -0300

    Update Categoria.php

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\tarea;
 
 class Categoria extends Model
 {
     use HasFactory;
+
+    public function tareas(){
+        return $this->hasMany(tarea::class);
+    }
 }
