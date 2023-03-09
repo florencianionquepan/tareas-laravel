15bbf2b901dc4df373cc29854558120300a1050e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-08 21:25:00 -0300

    Update show.blade.php

--- resources/views/categorias/show.blade.php
+++ resources/views/categorias/show.blade.php
@@ -4,7 +4,25 @@
 
 <div class="container w-25 border rounded border-primary my-5 p-2">
     <div class="row mx-auto">
+        <div>
+            @if($categoria->tareas->count()>0)
+                <div class="row py-2">
+                    <div class="col-md-9 d-flex align-items-center">
+                        <a href="{{ route('tareas-show', ['id' => $tarea->id] ) }}">{{$tarea->title}}</a>
+                      </div>
         
+                    <div class="col-md-3 d-flex justify-content-end">
+                      <form action="{{ route('tareas-destroy',[$tarea->id]) }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button class="btn btn-danger btn-sm">Eliminar</button>
+                      </form>
+                    </div>
+                </div>
+            @else
+                No hay tareas para la categoria
+            @endif
+        </div>
     </div>
 </div>
 
