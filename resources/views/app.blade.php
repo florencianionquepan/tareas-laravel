30553c9ba5385b221fecb26e65dd473ad506ee03
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-03-08 21:50:35 -0300

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,8 +20,8 @@
         
               <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                 <li><p class="nav-link px-2 text-secondary pb-1 mb-1">Mis tareas</p></li>
-                <li><a href="#" class="nav-link px-2 text-white">Categorias</a></li>
-                <li><a href="#" class="nav-link px-2 text-white">Tareas</a></li>
+                <li><a href="{{route('categorias.index')}}" class="nav-link px-2 text-white">Categorias</a></li>
+                <li><a href="{{route('tareas')}}" class="nav-link px-2 text-white">Tareas</a></li>
               </ul>
         
               <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
